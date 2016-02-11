113f5ac3ab167d1109f7cd6df0bc1b14a8534e47
Carlos Peña & Jean Rivas <gesaodin@gmail.com>
2016-02-11 15:29:25 -0430

    Vista detalles Apoyo

--- application/views/bienestarsocial/bienestar.php
+++ application/views/bienestarsocial/bienestar.php
@@ -4,34 +4,16 @@ $this->load->view("bienestarsocial/inc/cabecera.php");
 
 <br><br>
 <div class="container .hide-on-small-only">
- <div class="row">
 
-	 <div class="col s12">
-	 	<p>
-	 	Hola. Saludos, bienvenidos al sistema de reservación de citas para ayudas especializadas en:
-	 	Medicamentos de alto costo, Protesis, Operaciones y otros.
-	 	
-	 	Recuerde que debe cumplir con los siguientes requisitos según sea el caso
-	 	</p> 	 	
-	 </div>
- </div>
- 
  <div class="row">
  <div class="col s12">
- 	Requisitos 
- 	<ul> 		
- 		<li>1. Carta de exposición de motivos.</li>
- 		<li>2. Copia de la carta aval de seguros horizonte..</li>
- 		<li>3. Constancia Original de agotamiento de cobertura.</li>
- 		<li>4. Según sea el caso Constancia original de la deuda contraída.</li>
- 		<li>5. Copia de la cédula de identidad del afiliado.</li>
- 		<li>6. Informe médico firmado y sellado.</li>
- 		<li>7. Copia del carnets vigente del afiliado.</li>
- 		<li>8. Planilla carta compromiso.</li>
- 		<li>9. Planilla de solicitud de fondo de contingencia.</li>
- 	</ul>		
- </div>
- </div>
+<?php 
+  if($url == "ap"){
+    $this->load->view('bienestarsocial/imp/detApoyo');
+  }else{
+     $this->load->view('bienestarsocial/imp/detReembolso');
+  }
+?>
 
     <form class="col s12" 
       action="<?php echo base_url(); ?>index.php/BienestarSocial/imprimirHoja/<?php echo $url; ?>" 
