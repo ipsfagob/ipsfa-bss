c00b5c4b863326d71d9c61d12cf0486970584be1
Carlos Peña <gesaodin@gmail.com>
2016-02-19 18:19:15 -0430

    Detalles, Reembolso y Ayuda (html) Contenido

--- application/views/bienestarsocial/imp/detApoyo.php
+++ application/views/bienestarsocial/imp/detApoyo.php
@@ -1,19 +1,18 @@
-<p>
+<h4>Requisitos para la solicitud de Ayuda</h4>
+
+
 El instituto a través del fondo de cuidados integral de la salud, apoyará en un porcentaje razonable al personal profesional militar activo y retirado así como a sus familiares con derechos amparados por la ley de seguridad social de las Fuerza Armada Nacional, en aquellos casos que ameriten tratamiento médico quirúrgico y/u hospitalización, adquisición de material, prótesis, entre otros,  cuando no cuenten con el recurso financiero necesarios para solventar los gastos médicos incurridos por un excedente razonable en los centros de salud privado a nivel nacional, una vez que haya agotado la cobertura que ampara Seguros Horizonte y no sea atendido a través de la red sanitaria de la dirección general de salud de la Fan, para lo cual, deberá consignar los siguientes recaudos:
-<br><br>
 
-carta de exposición de motivos dirigidos al ciudadano Presidente de la Junta Administrativa del IPSFA.
-Copia de carta Aval emitida por Seguros Horizonte y/u otra compañía aseguradora.
-Constancia original de agotamiento de cobertura emitida por Seguros Horizontes y/u otra compañía de seguros.
-Constancia original de la deuda que mantenga el afiliado con la clínica, firmada y con sello húmedo elaborada por la administración del centro de salud Privada.
-Presupuesto de la clínica con sello Húmedo y Rif.
-Copia del informe Médico emitido por el hospital militar naval que certifique por escrito imposibilidad de atención médica temporal o definitiva en la red de Sanitaria Militar.
-Copia del informe médico actualizado y con sello húmedo del galeno.
-copia de la Cédula de Identidad del militar afiliado y del familiar con derecho ampliada y a color.
-Copia del carnet vigente del militar y del familiar con derecho ampliada a color.
-Planilla de Solicitud de “Apoyo Económico Social por Gastos Médicos “ cod. BSCSF220/ABRIL 2012.
-Planilla de carta compromiso de apoyo Económico Social.
-
-<br><br>
-DOY FE Y GARANTÍA DE  QUE LOS MOTIVOS DE LA PRESENTE SOLICITUD SON CIERTOS, EN TAL SENTIDO AUTORIZO A LA GERENCIA DE BIENESTAR Y SEGURIDAD SOCIAL PARA QUE REALICEN LAS DILIGENCIAS PERTINENTES A FIN DE CERTIFICAR LA VERACIDAD DE LOS RECAUDOS E INFORMACIÓN SUMINISTRADA. 
-</p>
+<ol>
+	<li>carta de exposición de motivos dirigidos al ciudadano Presidente de la Junta Administrativa del IPSFA.</li>
+	<li>Copia de carta Aval emitida por Seguros Horizonte y/u otra compañía aseguradora.</li>
+	<li>Constancia original de agotamiento de cobertura emitida por Seguros Horizontes y/u otra compañía de seguros.</li>
+	<li>Constancia original de la deuda que mantenga el afiliado con la clínica, firmada y con sello húmedo elaborada por la administración del centro de salud privada.</li>
+	<li>Presupuesto de la clínica con sello Húmedo y Rif.     </li>
+	<li>Copia del informe Médico emitido por el hospital militar naval que certifique por escrito imposibilidad de atención médica temporal o definitiva en la red de Sanitaria Militar.</li>
+	<li>Copia del informe médico actualizado y con sello húmedo del galeno.</la>
+	<li>copia de la Cédula de Identidad del militar afiliado y del familiar con derecho ampliada y a color.</li>
+	<li>Copia del carnet vigente del militar y del familiar con derecho ampliada a color.</li>
+	<li>Planilla de Solicitud de “Apoyo Económico Social por Gastos Médicos “ cod. BSCSF220/ABRIL 2012.</li>
+	<li>Planilla de carta compromiso de apoyo Económico Social.</li>
+</ol>
