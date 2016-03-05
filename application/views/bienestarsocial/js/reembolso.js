efbdfcf508e8320267699fbd93b6a69348b1659c
Carlos Peña <gesaodin@gmail.com>
2016-03-05 18:00:01 -0430

    Reembolso: Iniciar combo de concepto
    	modified:   application/views/bienestarsocial/js/reembolso.js

--- application/views/bienestarsocial/js/reembolso.js
+++ application/views/bienestarsocial/js/reembolso.js
@@ -9,20 +9,23 @@ var i = 0;
 
 
 
-
-
+/**
+* Agregando selección de productos a una lista virtual
+*
+* @return mixed
+*/
 function agregarR(){
 	var Pedido = {};
 	var familiar = $('#familiar').val();
 	var concepto = $('#concepto').val();
 	var monto = $('#monto').val();
 
-	if(monto == "0.00" || concepto == "0"){
+	if(monto == "0.00" || concepto == "0"){				
 		Materialize.toast("Debe introducir un monto o seleccionar un concepto", 3000, 'rounded');
+		iniciarCombos();
 	}else{
 		$('#total').val(parseFloat($('#total').val()) + parseFloat($('#monto').val()));
-		var linea = $('#concepto option:selected').text() + '|' + $('#monto').val();
-		
+		var linea = $('#concepto option:selected').text() + '|' + $('#monto').val();		
 
 		var arr = familiar.split('|');
 		
@@ -31,29 +34,25 @@ function agregarR(){
 		Pedido['nombre'] = $('#familiar option:selected').text();
 		Pedido['concepto'] = $('#concepto option:selected').text();
 		Pedido['monto'] = $('#monto').val();
-
-
 		Solicitud[i++] = Pedido;
-		//numero = formatNumber.new($('#monto').val());
-		
-		$('#htotal').html('Total ' + $('#total').val() + ' Bs.');
-		
+		$('#htotal').html('Total ' + $('#total').val() + ' Bs.');		
 		cadena = '<i class="material-icons red circle tooltipped waves-effect waves-light"' + 
 		' onclick="eliminarR(' + i + ')" title="Eliminar Pedido">delete</i>';
 		cadena += '<span class="title">' + $('#concepto option:selected').text();
 		cadena += '</sapn><p>' + $('#familiar option:selected').text() ;
 		cadena += '<br>MONTO: ' + $('#monto').val() + '</p>';
 		
-		$('#dtReembolso').append('<li class="collection-item avatar" id="' + i + '">' + cadena + '</li>');
-		$('#monto').val('0.00');
+		$('#dtReembolso').append('<li class="collection-item avatar" id="' + i + '">' + cadena + '</li>');		
+		iniciarCombos();
 	}
 }
 
-function mensaje(){
-	$('#modal1').openModal();
+
+function iniciarElementos(){
+	$('#monto').val('0.00');
+	$('#concepto').val("0");
+	$('select').material_select();
 }
-
-
 
 function salvarR(codigo){
 	var Reembolso = {};
