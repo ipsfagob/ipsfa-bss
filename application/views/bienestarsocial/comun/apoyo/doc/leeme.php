c0ccb85379bd10edb7f10825dcaf2b647c0eb3f4
Carlos Peña <gesaodin@gmail.com>
2016-04-01 16:16:03 -0430

    Apoyo: doc:leame

--- application/views/bienestarsocial/comun/apoyo/doc/leeme.php
+++ application/views/bienestarsocial/comun/apoyo/doc/leeme.php
@@ -1,17 +1,17 @@
 <h4>Requisitos para la solicitud de Ayuda</h4>
 
 
-El instituto a través del fondo de cuidados integral de la salud, apoyará en un porcentaje razonable al personal profesional militar activo y retirado así como a sus familiares con derechos amparados por la ley de seguridad social de las Fuerza Armada Nacional, en aquellos casos que ameriten tratamiento médico quirúrgico y/u hospitalización, adquisición de material, prótesis, entre otros,  cuando no cuenten con el recurso financiero necesarios para solventar los gastos médicos incurridos por un excedente razonable en los centros de salud privado a nivel nacional, una vez que haya agotado la cobertura que ampara Seguros Horizonte y no sea atendido a través de la red sanitaria de la dirección general de salud de la Fan, para lo cual, deberá consignar los siguientes recaudos:
+El instituto a través del fondo de cuidado integral de la salud, apoyará en un porcentaje razonable al personal profesional militar activo y retirado así como a sus familiares con derechos amparados por la ley de seguridad social de las Fuerza Armada Nacional, en aquellos casos que ameriten tratamiento médico quirúrgico y/u hospitalización, adquisición de material, prótesis, entre otros,  cuando no cuenten con el recurso financiero necesarios para solventar los gastos médicos incurridos por un excedente razonable en los centros de salud privado a nivel nacional, una vez que haya agotado la cobertura que ampara Seguros Horizonte y no sea atendido a través de la red sanitaria de la dirección general de salud de la FAN, para lo cual, deberá consignar los siguientes recaudos:
 
 <ol>
-	<li>carta de exposición de motivos dirigidos al ciudadano Presidente de la Junta Administrativa del IPSFA.</li>
+	<li>Carta de exposición de motivos dirigidos al ciudadano Presidente de la Junta Administrativa del IPSFA.</li>
 	<li>Copia de carta Aval emitida por Seguros Horizonte y/u otra compañía aseguradora.</li>
 	<li>Constancia original de agotamiento de cobertura emitida por Seguros Horizontes y/u otra compañía de seguros.</li>
 	<li>Constancia original de la deuda que mantenga el afiliado con la clínica, firmada y con sello húmedo elaborada por la administración del centro de salud privada.</li>
-	<li>Presupuesto de la clínica con sello Húmedo y Rif.     </li>
-	<li>Copia del informe Médico emitido por el hospital militar naval que certifique por escrito imposibilidad de atención médica temporal o definitiva en la red de Sanitaria Militar.</li>
+	<li>Presupuesto de la clínica con sello húmedo y rif.     </li>
+	<li>Copia del informe médico emitido por el hospital militar naval que certifique por escrito imposibilidad de atención médica temporal o definitiva en la red de sanitaria militar.</li>
 	<li>Copia del informe médico actualizado y con sello húmedo del galeno.</la>
-	<li>copia de la Cédula de Identidad del militar afiliado y del familiar con derecho ampliada y a color.</li>
+	<li>Copia de la cédula de identidad del militar afiliado y del familiar con derecho ampliada y a color.</li>
 	<li>Copia del carnet vigente del militar y del familiar con derecho ampliada a color.</li>
 	<li>Planilla de Solicitud de “Apoyo Económico Social por Gastos Médicos “ cod. BSCSF220/ABRIL 2012.</li>
 	<li>Planilla de carta compromiso de apoyo Económico Social.</li>
